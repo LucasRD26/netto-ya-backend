399f2badb056740c47ea6b2f1db99681393e8085
LucasRD26 <lucas.rodriguez@est.iudigital.edu.co>
2025-06-02 10:04:15 -0500

    first commit

---
+++ src/main/java/com/nettoya/NettoYaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nettoya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettoYaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettoYaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nettoya/NettoYaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nettoya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettoYaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
